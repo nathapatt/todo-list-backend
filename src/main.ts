d61bff75fdf300f10e92222a73a77bb9db48b1a1
nathapatt <nathapat.mind@gmail.com>
2025-08-05 15:01:55 +0700

    addProxy

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // <--- Add this line
-  app.enableCors({
-    origin: 'http://localhost:5173', // ให้เฉพาะ Frontend localhost:5173 เข้าถึงได้
-    credentials: true, // ถ้าใช้ cookie ให้ใส่ด้วย (optional)
-  });
+  // app.enableCors({
+  //   // origin: 'http://localhost:5173', // ให้เฉพาะ Frontend localhost:5173 เข้าถึงได้
+  //   credentials: true, // ถ้าใช้ cookie ให้ใส่ด้วย (optional)
+  // });
 
   await app.listen(3000);
 }
